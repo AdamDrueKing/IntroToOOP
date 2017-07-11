46186776d5d5b7734da635419ff4cef032069e69
Adam King <adam81king@gmail.com>
2017-07-11 10:08:14 -0400

    Created Cat class

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -11,5 +11,47 @@ namespace IntroToOOP
         //Inside of our class, we create the blueprint for our object.
         //Classes are templates in OOP that we use to create objects.
         //The should always be named in Pascal case.
+
+        //In OOP, there are two types of States:
+        //Fields - variables that belong to a class
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //Properties - a sort of mix between a field and a method that allow controlled access
+        public string Name
+            {
+            get {return this.name; }
+            set { this.name = value; }
+            }
+
+
+        //We also have Behaviors.
+        //Methods - repeatable/reusable sections of code - Actions
+        //Constructors - specialized methods that are used when instantiating an object
+        //  -allow us to control the state of an object upon creation
+        //  /allow us to perform any actions of the object might need
+        //  -always share the name of the class
+        public Cat()
+        {
+            //This is an example of a default constructor. A default constructor takes no arguments/parameters
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        public void Eat()
+        {
+            if(isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat Hungry? " + isHungry);
+        }
     }
 }
