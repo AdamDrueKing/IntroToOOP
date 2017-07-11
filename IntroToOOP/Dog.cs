77a3a3c016f385895f9e31130afdced645e33d05
Adam King <adam81king@gmail.com>
2017-07-11 11:57:24 -0400

    Built out Dog class

---
+++ IntroToOOP/Dog.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class Dog
+    {
+        //Create a Dog class
+        //Your dog class should have fields for hair length, height, running speed and weight
+        //Your class should have the following behaviors: run, bark, potty, cuddle
+
+        //Fields:
+        private string hairLength;
+        private int height;
+        private int runningSpeed;
+        private double weight;
+
+        //Properties
+        public string HairLength
+        {
+            get { return this.hairLength; }
+            set { this.hairLength = value; }
+        }
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public int RunningSpeed
+        {
+            get { return this.runningSpeed; }
+            set { this.weight = value; }
+        }
+
+        //****public int RunningSpeed {get; set;} does the same thing as above
+
+        //Constructor:
+        public Dog()
+        {
+            //this is my default constructor
+        }
+
+        public Dog(string hairLength, int height, int runningSpeed, double weight)
+        {
+            this.hairLength = hairLength;
+            this.height = height;
+            this.runningSpeed = runningSpeed;
+            this.weight = weight;
+        }
+
+
+        //Behaviors:
+        public int Run()
+        {
+        Console.WriteLine("Go doggy go!");
+            RunningSpeed -= 1;
+            return RunningSpeed;
+            }
+
+        public void Bark()
+        {
+        Console.WriteLine("Bork bork bork!!!");
+        }
+
+        public void Potty()
+        {
+            Console.WriteLine("Fart noises!!!");
+        }
+
+        public void Cuddle()
+        {
+            Console.WriteLine("Awww, doggo is so sweet.");
+        }
+
+        
+
+
+    }
+}
--- IntroToOOP/Program.cs
+++ IntroToOOP/Program.cs
@@ -20,15 +20,24 @@ namespace IntroToOOP
             //ClassName objectName = new ClassName();
             Cat mittens = new Cat();
 
+            //firstCat.Name = "Mittens";
+            //Console.WriteLine(firstCat.Age);
+
+            Cat secondCat = new Cat("Oz", 3, "Orange and white");
+            secondCat.Name = "Ozzy";
+            Console.WriteLine(secondCat.Name);
+            Console.WriteLine(secondCat.Age);
+            secondCat.Eat();
+
             //To create a new object, we use a special method called a CONSTRUCTOR
 
             //Thinking about objects in real life, create (either hand-write or type)...
             //...three examples of objects. Your objects should have five states (characteristics...
             //...and five behaviors.
 
-            Backpack(States: straps, compartments, color, padding, material); (Behaviors: holding things, being worn, allowing hand freedom,)
-            Water bottle(States: cap, volume, label, color, texture); (Behaviors: holding water, standing upright,   )
-            S
+            //Backpack(States: straps, compartments, color, padding, material); (Behaviors: holding things, being worn, allowing hand freedom,)
+            //Water bottle(States: cap, volume, label, color, texture); (Behaviors: holding water, standing upright,   )
+            //S
         }
     }
 }
